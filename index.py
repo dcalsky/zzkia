f18bfa62266ceadd604d6f256d28503a5b5019d8
dcalsky <dcalsky@163.com>
2019-10-10 21:58:01 +0800

    add default entrance

--- index.py
+++ index.py
@@ -5,7 +5,11 @@ from sanic.response import raw
 app = Sanic()
 
 
-@app.get("/<text:string>")
-async def index(request, text: str):
+@app.get("/<text>")
+async def generate(request, text: str):
     return raw(generate_image(text), content_type="image/png")
 
+
+@app.get("/")
+async def index(request):
+    return raw(generate_image("ZZKIA"), content_type="image/png")
