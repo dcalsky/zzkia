8dedb68fe5d69164e8ff29c6384252a18e82660a
dcalsky <dcalsky@163.com>
2019-10-10 22:20:46 +0800

    change all get routes to post

--- index.py
+++ index.py
@@ -5,11 +5,11 @@ from sanic.response import raw
 app = Sanic()
 
 
-@app.get("/<text>")
+@app.post("/<text>")
 async def generate(request, text: str):
     return raw(generate_image(text), content_type="image/png")
 
 
-@app.get("/")
+@app.post("/")
 async def index(request):
     return raw(generate_image("ZZKIA"), content_type="image/png")
