445c40acb0319c46dd170028668b19ae63d94305
dcalsky <dcalsky@163.com>
2019-10-11 14:13:06 +0800

    move generator to api folder

--- api/nokia.py
+++ api/nokia.py
@@ -1,14 +1,19 @@
 import textwrap
+import base64
 import io
+import os
 from typing import Tuple
 from PIL import Image, ImageFont, ImageDraw, ImageOps
 
 font_size = 70
 line_gap = 20
 line_width = 9
-origin_pos = (260, 340)
-font_color = (0, 0, 0, 255)
+body_pos = (260, 340)
+subtitle_pos = (790, 320)
+body_color = (0, 0, 0, 255)
+subtitle_color = (129, 212, 250, 255)
 line_rotate = -9.1
+font = ImageFont.truetype("api/fonts/1.ttf", font_size)
 
 
 def image_to_byte_array(image: Image):
@@ -18,35 +23,41 @@ def image_to_byte_array(image: Image):
     return imgByteArr
 
 
+def im_2_b64(image):
+    buff = io.BytesIO()
+    image.save(buff, format="PNG")
+    img_str = base64.b64encode(buff.getvalue())
+    return img_str
+
+
 def draw_subtitle(im, text: str):
-    font = ImageFont.truetype("fonts/1.ttf", 70)
+
     width, height = font.getsize(text)
     image2 = Image.new("RGBA", (width, height))
     draw2 = ImageDraw.Draw(image2)
-    draw2.text((0, 0), text=text, font=font, fill=(129, 212, 250, 255))
+    draw2.text((0, 0), text=text, font=font, fill=subtitle_color)
     image2 = image2.rotate(line_rotate, expand=1)
 
-    px, py = (790, 320)
+    px, py = subtitle_pos
     sx, sy = image2.size
     im.paste(image2, (px, py, px + sx, py + sy), image2)
 
 
 def generate_image(text: str, path=None):
-    im = Image.open(path or "images/3.png")
+    im = Image.open(path or "api/images/3.png")
     length = len(text)
-    font = ImageFont.truetype("fonts/1.ttf", font_size)
     width, height = font.getsize(text)
     lines = textwrap.wrap(text, width=line_width)
     image2 = Image.new("RGBA", (width, (height + line_gap) * len(lines)))
     draw2 = ImageDraw.Draw(image2)
     for i, line in enumerate(lines):
-        draw2.text((0, i * (height + line_gap)), text=line, font=font, fill=font_color)
+        draw2.text((0, i * (height + line_gap)), text=line, font=font, fill=body_color)
     image2 = image2.rotate(line_rotate, expand=1)
 
-    px, py = origin_pos
+    px, py = body_pos
     sx, sy = image2.size
     im.paste(image2, (px, py, px + sx, py + sy), image2)
     draw_subtitle(im, f"{length}/900")
     # im.save("results/final.png", "png")
-    return image_to_byte_array(im)
+    return im_2_b64(im)
 
